08db0a2a6f17b12fc068974a792b1b26dd9b68ca
propulse-7969 <omutkarsh2005@gmail.com>
2025-03-13 21:41:19 +0530

    checks

--- gen_ai.py
+++ gen_ai.py
@@ -1,7 +1,8 @@
 from google import genai
 from google.genai import types
+import creds
 
-GEMINI_API_KEY = "AIzaSyBIOGYDY9jYGHqd0mE6PZyf1SCPgNC5Iqw"
+GEMINI_API_KEY = "creds.GEMINI_API_KEY"
 
 sys_instruct="You are an advanced humanoid AI way, competing Alexa and Siri, you are like Jarvis and your name is STELLA. Answer to my commands in a humane way and also apt and to the point."
 client = genai.Client(api_key=f"{GEMINI_API_KEY}")
